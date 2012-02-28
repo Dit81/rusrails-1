edeac2470a81030d6d4b7aa4af8e9299709fc3fc
yaxinr <yaxinr@gmail.com>
2012-02-28 10:46:30 +0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rusrails::Application.config.secret_token = '578920ba8e690867a0a90781b41844421d77b987a50346dee442f1281a475de80893fab2196453cda1a20d7f43538ecca449a7bda4b68ad792a61a0da5647def'
---
+++ db/migrate/20120210092808_add_details_to_products.rb
@@ -0,0 +1,8 @@
+class AddDetailsToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :part_number, :string
+
+    add_column :products, :price, :decimal
+
+  end
+end
